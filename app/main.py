61230077ec3d86263afd117b790299ec380cafdd
yijunx <yijunx@umich.edu>
2024-06-23 02:51:05 +0000

    update app

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return {
+        "hello": "its up"
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
